130acd7f86bf9208b896996183367bbb8c4f5927
Dave Slotter <w3djs@arrl.net>
2020-05-13 15:25:12 -0400

    Initial commit.

---
+++ files/boinc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+boinccmd --acct_mgr attach https://bam.boincstats.com/ "169373_cb27b99639409722eb67cc774b50e1c1" "169373_cb27b99639409722eb67cc774b50e1c1"
---
+++ files/qslware.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export TEXTDOMAIN=pprompt
+
+zenity --warning --width=400 --title "QSLWARE NOTICE" --timeout=59 --text "Please note that HamPi is QSLware. If you use it, please send me (W3DJS) a QSL card. My address is on QRZ.com.\n\nOnce I receive your QSL card, I will send instructions on how to remove this dialog.\n\nBy operating this image, you agree to the terms and conditions in the license agreement."
