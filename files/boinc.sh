7e1a0d2473148650baa2d06b918176adfb2d4ef4
Dave Slotter <w3djs@arrl.net>
2020-06-13 14:37:46 -0400

    Delay for network...

--- files/boinc.sh
+++ files/boinc.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+sleep 60 # Give network time to come online...
+
 boinccmd --acct_mgr attach https://bam.boincstats.com/ "169373_cb27b99639409722eb67cc774b50e1c1" "169373_cb27b99639409722eb67cc774b50e1c1"
