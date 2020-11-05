0536754bcf1033f8fb901e96fc7b76302820cadb
Dave Slotter <w3djs@arrl.net>
2020-11-05 14:39:01 -0500

    Make notice more specific.

--- files/home/pi/.config/lxsession/LXDE-pi/qslware.sh
+++ files/home/pi/.config/lxsession/LXDE-pi/qslware.sh
@@ -2,4 +2,4 @@
 
 export TEXTDOMAIN=pprompt
 
-zenity --warning --width=400 --title "QSLWARE NOTICE" --timeout=59 --text "Please note that HamPi is QSLware. If you use it, please send me (W3DJS) a QSL card. My address is on QRZ.com.\n\nOnce I receive your QSL card, I will send instructions on how to remove this dialog.\n\nBy operating this image, you agree to the terms and conditions in the license agreement."
+zenity --warning --width=400 --title "QSLWARE NOTICE" --timeout=59 --text "Please note that HamPi is QSLware. If you use it, please send me (W3DJS) a QSL card by postal mail. My address is on QRZ.com.\n\nOnce I receive your QSL card by mail, I will email instructions on how to remove this dialog. So please include your email address on the QSL card.\n\nBy operating this image, you agree to the terms and conditions in the license agreement."
