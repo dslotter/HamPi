cc5c40af5e492150957f0894a0f6b3271f902816
Dave Slotter <w3djs@arrl.net>
2021-12-31 18:57:33 -0500

    Do not overload low-memory machines.

--- files/home/pi/.config/lxsession/LXDE-pi/boinc.sh
+++ files/home/pi/.config/lxsession/LXDE-pi/boinc.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Check for less than 2GB RAM
+if [ $MEM -lt 2097152 ]; then
+  echo "Not enough memory to proceed..."
+  exit 0
+fi
+
 sleep 60 # Give network time to come online...
 
 boinccmd --acct_mgr attach https://bam.boincstats.com/ "169373_cb27b99639409722eb67cc774b50e1c1" "169373_cb27b99639409722eb67cc774b50e1c1"
