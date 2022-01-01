f78832a6b4da342cdb9c134ab36aefbfc0590352
Dave Slotter <w3djs@arrl.net>
2021-12-31 21:23:10 -0500

    fine-tuning

--- files/home/pi/.config/lxsession/LXDE-pi/boinc.sh
+++ files/home/pi/.config/lxsession/LXDE-pi/boinc.sh
@@ -6,6 +6,8 @@ if [ $MEM -lt 2097152 ]; then
   exit 0
 fi
 
+sudo -u root /etc/init.d/boinc-client start
+
 sleep 60 # Give network time to come online...
 
 boinccmd --acct_mgr attach https://bam.boincstats.com/ "169373_cb27b99639409722eb67cc774b50e1c1" "169373_cb27b99639409722eb67cc774b50e1c1"
